5b9747a6aebc51e107849bf4c2fa4065cbdca9e1
Adarsh Thakur <adarsh.thakur@deskera.com>
2023-01-11 21:39:50 +0530

    Fallback for BDR

--- src/chat/BDRInfo.tsx
+++ src/chat/BDRInfo.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { GUEST_USER_COOKIE } from "../Utility/Constants";
 import { getCookie } from "../Utility/Utility";
+import ic_bdr from "../assets/images/ic_bdr.png";
 export enum INITIAL_USER_SELECTION {
     TALK_TO_REP = `I want to talk to a Specialist 🙋‍♂️`,
     BOOK_A_MEET = `I want to schedule a meeting`
@@ -11,14 +12,18 @@ function BDRInfo(props) {
             <div className="dk-chat-d-flex dk-chat-align-items-center dk-chat-parent-width dk-chat-mb-r" style={{ gap: 10, flexDirection: 'column' }}>
                 <div className="dk-chat-bg-chip-orange dk-chat-text-blue dk-chat-fs-r dk-chat-fw-b dk-chat-p-h-l dk-chat-border-radius-m dk-chat-text-orange">Meet your sales representative</div>
                 <div
-                    className="dk-chat-bg-chip-orange dk-chat-bdr-profile"
+                    className="dk-chat-bg-chip-orange"
                     style={{
                         width: 100,
                         height: 100,
                         borderRadius: '50%',
-                        backgroundImage: props.bdrInfo?.profilePic && `url(${props.bdrInfo?.profilePic})`,
                         backgroundRepeat: 'round'
-                    }}></div>
+                    }}>
+                    <img className="dk-chat-parent-size" style={{ borderRadius: '50%' }} src={`${props.bdrInfo?.profilePic}`} alt="" onError={({ currentTarget }:any) => {
+                        currentTarget.onerror = null;
+                        currentTarget.src = ic_bdr;
+                    }} />
+                </div>
                 <div className="dk-chat-fw-b dk-chat-text-align-center">
                     <div className="dk-chat-fs-l">{props.bdrInfo?.displayName}</div>
                     <div className="dk-chat-fs-r dk-chat-text-gray">{props.bdrInfo?.email}</div>
